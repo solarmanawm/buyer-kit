0a2943829c079c6ce1ea90d05c5d1ca44627dc99
DmitryF <dfisunov@srt-llc.ru>
2023-08-02 18:46:28 +0300

    Start

---
+++ src/App.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>App</p>
+</template>
---
+++ src/components/Row/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <p>Row</p>
+</template>
+
+<script lang="ts">
+interface T {
+    prop: string;
+}
+
+const t: T = {
+    prop: 'string',
+};
+
+console.log(t);
+
+export default {
+    name: 'VRow',
+};
+</script>
