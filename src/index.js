9cde5b6558abd3485eaeb95d17ef83252837ce08
DmitryF <dfisunov@srt-llc.ru>
2023-08-03 18:53:34 +0300

    Fix path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import VRow from '@/components/VRow/index.vue';
-import VContainer from '@/components/VContainer/index.vue';
-import VColumn from '@/components/VColumn/index.vue';
+import VRow from './components/VRow/index.vue';
+import VContainer from './components/VContainer/index.vue';
+import VColumn from './components/VColumn/index.vue';
 
 const BuyerKit = {
     // eslint-disable-next-line no-unused-vars
