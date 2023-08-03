d92c0c4b27fd0c622d26ab100db01a3cab9d1dd7
DmitryF <dfisunov@srt-llc.ru>
2023-08-03 18:42:13 +0300

    Make a plugin

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import VRow from '@/components/VRow/index.vue';
+import VContainer from '@/components/VContainer/index.vue';
+import VColumn from '@/components/VColumn/index.vue';
+
+const BuyerKit = {
+    // eslint-disable-next-line no-unused-vars
+    install(Vue, options) {
+        Vue.component('VRow', VRow);
+        Vue.component('VContainer', VContainer);
+        Vue.component('VColumn', VColumn);
+    }
+  };
+  
+  export default BuyerKit;
